593fba5e2871b9f8be0de1183923f4fe7577f329
Rafal <rgrajcar@gmail.com>
2024-12-17 22:45:25 +0100

    added counter state

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,6 +1,9 @@
 import styles from "./style.module.scss";
+import {useState} from "react";
 
 export const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className={styles.counter}>
       0
