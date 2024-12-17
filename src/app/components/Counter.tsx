e2995cbe442f6ea3c91f880a27bde1feac3b6a03
Rafal <rgrajcar@gmail.com>
2024-12-17 22:46:44 +0100

    added button and increment method to set new counter value

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -4,9 +4,14 @@ import {useState} from "react";
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  const increment = () => {
+    setCounter(prev => prev+1);
+  }
+
   return (
     <div className={styles.counter}>
-      0
+      {counter}
+      <button onClick={increment}>+1</button>
     </div>
   )
 }
