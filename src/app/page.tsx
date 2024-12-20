059463434d69433d5f591eb95eb6049d5077ab78
Rafal <rgrajcar@gmail.com>
2024-12-20 11:46:21 +0100

    added Container to wrap components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import {Counter} from "@/app/components/Counter";
+import {Container} from "@/app/components/Container/Container";
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Counter />
       <h1>Hello world</h1>
-    </>
+    </Container>
   );
 }
